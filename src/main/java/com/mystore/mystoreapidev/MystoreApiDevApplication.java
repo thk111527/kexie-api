0fc18b6ced93c0d9abf4e221ea01617a20214d72
HANLI <hanlihlhl2020@outlook.com>
2022-08-01 15:19:02 +0800

    init project

---
+++ src/main/java/com/mystore/mystoreapidev/MystoreApiDevApplication.java
@@ -0,0 +1,13 @@
+package com.mystore.mystoreapidev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystoreApiDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MystoreApiDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mystore/mystoreapidev/MystoreApiDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystore.mystoreapidev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystoreApiDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
